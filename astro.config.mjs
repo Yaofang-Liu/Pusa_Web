5f5359a5d06da3a6686bcda9aceb3c6dad4e0411
Yaofang-Liu <lyf02zz11@gmail.com>
2025-07-14 08:53:06 +0800

    update name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: "https://Yaofang-Liu.github.io",
-  base: '/Pusa_Web/',
+  base: '/',
 
   vite: {
     plugins: [tailwindcss()],
